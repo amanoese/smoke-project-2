a375e6ecc8b13793af9e3e058bd4b24785dcd94a
amanoese <amanoese@gmail.com>
2016-08-15 18:00:22 +0000

    out file test

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var Cylon = require('cylon');
+var json2csv = require('json2csv');
+var fs = require('fs');
 
 Cylon.robot({
   connections: {
@@ -11,6 +13,8 @@ Cylon.robot({
   },
 
   work: function(my) {
+    var fileName = `./outfile/outfile-${Date.now()}.csv`;
+    fs.writeFile(fileName,'temperature\n');
     every((2).seconds(), function() {
       my.bmp180.getAltitude(1, null, function(err, val) {
         if (err) {
@@ -20,6 +24,7 @@ Cylon.robot({
         var ledBrite = parseInt(val.temp.fromScale(30,33).toScale(0, 255));
         my.led.brightness(ledBrite);
         console.log("\tledBrite: " + ledBrite,"\tTemperature: " + val.temp + " C","\tPressure: " + val.press + " Pa", "\tAltitude: " + val.alt + " m");
+        fs.appendFile(fileName,val.temp + '\n');
       });
     });
   }
